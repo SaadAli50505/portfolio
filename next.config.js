f7b9dd7754d3d4ddd98b27adde82f15387cf4d26
Saad Ali <saad.techling@gmail.com>
2025-04-13 21:03:08 +0500

    image issue fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // <-- this disables next/image optimization
+  },
 };
 
 module.exports = nextConfig;
